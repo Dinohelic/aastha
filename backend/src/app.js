72a47fb4d104da5ca3a6bc7c93cfbcd120171d98
Dinohelic <dy579962@gmail.com>
2026-07-27 05:08:50 +0530

    Temporary open CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,14 +15,7 @@ const app = express();
 import cors from "cors";
 
 app.use(cors({
-    origin: [
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "https://adubeaastha.netlify.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    origin: "*"
 }));
 
 app.use(express.json());
