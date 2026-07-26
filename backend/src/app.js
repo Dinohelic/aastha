61aed1cd40153eee74bd5a2aa1782cea04293518
Dinohelic <dy579962@gmail.com>
2026-07-27 05:13:28 +0530

    Cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cors({
     origin: [
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://adubeaastha.netlify.app"
+        "https://adubeaastha.netlify.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
